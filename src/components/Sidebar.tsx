3d6b4f408f476d3d87757c2fba7df69b7ddfc0a2
araaiko <araaiko.122@gmail.com>
2022-12-01 23:37:04 +0900

    複数のSuspenseを組み合わせる

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+/** 外部import */
+import { FC } from "react";
+
+export const Sidebar: FC = () => {
+  return (
+    <div style={{ width: "400px", border: "2px solid gray" }}>
+      <a href="dummy">TOP</a>
+      <br />
+      <br />
+      <a href="dummy">マイページ</a>
+      <br />
+      <br />
+      <a href="dummy">利用規約</a>
+    </div>
+  );
+};
